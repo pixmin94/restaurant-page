b03c321e0de9002ea87142a34953ff4c7def948c
corina <pixmin@outlook.com>
2022-08-14 11:57:06 +0800

    Added header element

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,32 @@
-alert("good job!");
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("// alert(\"hello!\")\n\nfunction createHeader() {\n    const header = document.createElement(\"header\");\n    header.classList.add(\"header\");\n  \n    const restaurantName = document.createElement(\"h1\");\n    restaurantName.classList.add(\"restaurant-name\");\n    restaurantName.textContent = \"ChakoEats\";\n  \n    header.appendChild(restaurantName);\n    console.log(header)\n  \n    const content = document.getElementById(\"content\");\n    console.log(content)\n    content.appendChild(header);\n  }\n\n  createHeader();\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert("good job!")
+// alert("hello!")
+
+function createHeader() {
+    const header = document.createElement("header");
+    header.classList.add("header");
+  
+    const restaurantName = document.createElement("h1");
+    restaurantName.classList.add("restaurant-name");
+    restaurantName.textContent = "ChakoEats";
+  
+    header.appendChild(restaurantName);
+    console.log(header)
+  
+    const content = document.getElementById("content");
+    console.log(content)
+    content.appendChild(header);
+  }
+
+  createHeader();
