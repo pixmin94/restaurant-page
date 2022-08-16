a15f5879e65d3b0663d002b779eca8874402ce71
corina <pixmin@outlook.com>
2022-08-16 22:05:51 +0800

    Add nav bar with buttons

--- src/index.js
+++ src/index.js
@@ -22,11 +22,36 @@ function createHeader() {
     return main;
   }
 
-  function initializeWebsite(){
+  function createNavBar(id) {
+    const nav = document.createElement('nav');
+    nav.setAttribute('id', id);
+
+    const homeBtn = createBtn('home', 'home');
+    const menuBtn = createBtn('menu', 'menu');
+    const contactBtn = createBtn('contact', 'contact');
+
+    nav.appendChild(homeBtn);
+    nav.appendChild(menuBtn);
+    nav.appendChild(contactBtn);
+    return nav;
+  }
+
+  function createBtn(id, text) {
+    const btn = document.createElement('button');
+    btn.setAttribute('id', id);
+    const span = document.createElement('span');
+    span.textContent = text;
+    btn.appendChild(span);
+    btn.classList.add('tab');
+    return btn;
+  }
+
+  function init(){
     const content = document.getElementById("content");
     console.log(content)
     content.appendChild(createHeader());
+    content.appendChild(createNavBar());
     content.appendChild(createMain());
   }
 
-  initializeWebsite();
+  init();
