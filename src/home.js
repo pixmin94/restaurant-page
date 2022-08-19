ddbe399702b6b94a90038a3262a670d094d111de
corina <pixmin@outlook.com>
2022-08-19 23:30:53 +0800

    Added module for loading home page contents

--- src/home.js
+++ src/home.js
@@ -1,6 +1,18 @@
+function createIntro(){
+    const intro = document.createElement("section")
+    intro.classList.add("section")
+    intro.textContent = "Welcome to the New Jeans Fanclub page!"
+
+    const groupPic = document.createElement("img")
+    groupPic.classList.add("group-pic")
+    groupPic.setAttribute("src", "../src/img/grouppic.jpeg")
+
+    intro.appendChild(groupPic)
+    return intro;
+}
 
 function loadHome() {
     const content = document.getElementById("main");
-    content.textContent = "test";
+    content.appendChild(createIntro());
 }
 export default loadHome;
--- src/index.js
+++ src/index.js
@@ -64,3 +64,4 @@ function createHeader() {
   }
 
   init();
+  loadHome();
